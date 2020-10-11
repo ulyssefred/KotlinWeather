d03d53e74577ff7ebcb3d0505db43b9c2b827313
ulysses <wx2806784370@gmail.com>
2020-10-11 21:31:41 +0800

    release

--- app/src/main/java/com/example/kotlinweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/kotlinweather/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlinweather.MainActivity
 import com.example.kotlinweather.R
 import com.example.kotlinweather.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -28,7 +29,7 @@ class PlaceFragment:Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if (viewModel.isPlaceSaved()){
+        if (activity is MainActivity && viewModel.isPlaceSaved()){
             val place  = viewModel.getSavedPlace()
             val intent = Intent(context, WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
--- app/src/main/java/com/example/kotlinweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/kotlinweather/ui/weather/WeatherActivity.kt
@@ -1,13 +1,18 @@
 package com.example.kotlinweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlinweather.R
@@ -48,8 +53,35 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this,"无法获得天气信息" , Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeWeather.isRefreshing = false
         })
+        swipeWeather.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipeWeather.setOnRefreshListener {
+            refreshWeather()
+        }
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {
+            }
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+            }
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
+    }
+    fun refreshWeather(){
         viewModel.refreshWeather(viewModel.locationLng,viewModel.locationLat)
+        swipeWeather.isRefreshing = true
     }
     private fun showWeatherInfo(weather :Weather){
         placeName.text = viewModel.placeName
